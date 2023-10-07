d1e54dc7d23f028a2b23a89f3c175042ee189cec
samokun-nova <mokunnova@gmail.com>
2023-10-07 08:42:59 -0700

    initial-commit

---
+++ Documents/assignment1/src/useDate.js
@@ -0,0 +1,26 @@
+
+import { useState } from 'react';
+
+function useDate(initialDate) {
+  const [date, setDate] = useState(initialDate);
+
+  const addDays = (daysToAdd) => {
+    const newDate = new Date(date);
+    newDate.setDate(date.getDate() + daysToAdd);
+    setDate(newDate);
+  };
+
+  const addMonths = (monthsToAdd) => {
+    const newDate = new Date(date);
+    newDate.setMonth(date.getMonth() + monthsToAdd);
+    setDate(newDate);
+  };
+
+  return {
+    date,
+    addDays,
+    addMonths,
+  };
+}
+
+export default useDate;
